bb64056feede522478f8cbb5b6199cdc7415767b
K2 <115832286+KriVaTri@users.noreply.github.com>
2025-11-04 10:21:34 +0100

    Refactor configuration keys in const.py

--- custom_components/solardelta/const.py
+++ custom_components/solardelta/const.py
@@ -6,15 +6,11 @@ DOMAIN = "solardelta"
 PLATFORMS: list[Platform] = [Platform.SENSOR]
 
 # Config keys
-CONF_SOLAR_ENTITY = "solar_entity"
-CONF_DEVICE_ENTITY = "device_entity"
-CONF_DEVICE_ENTITIES = "device_entities"  # legacy compat (list)
 CONF_NAME = "name"
+CONF_SOLAR_ENTITY = "solar_entity"
+CONF_GRID_ENTITY = "grid_entity"
+CONF_DEVICE_ENTITY = "device_entity"
 CONF_STATUS_ENTITY = "status_entity"
 CONF_STATUS_STRING = "status_string"
 CONF_RESET_ENTITY = "reset_entity"
 CONF_RESET_STRING = "reset_string"
-
-# Legacy keys for backward compatibility (do not use in new code)
-LEGACY_CONF_TRIGGER_ENTITY = "trigger_entity"
-LEGACY_CONF_TRIGGER_STRING_1 = "trigger_string_1"
